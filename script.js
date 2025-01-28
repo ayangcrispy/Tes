25d0fb6c0381e459d7d6ef0561cddaa659f2f756
ayangcrispy <147887485+ayangcrispy@users.noreply.github.com>
2025-01-28 16:25:52 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+   if ('serviceWorker' in navigator) {
+       window.addEventListener('load', () => {
+           navigator.serviceWorker.register('/service-worker.js').then(registration => {
+               console.log('Service Worker registered with scope:', registration.scope);
+           }).catch(error => {
+               console.error('Service Worker registration failed:', error);
+           });
+       });
+   }
+
+
+
+
+
+
+
+
+
 
 const upload1 = document.getElementById('upload-1');
 const canvas1 = document.getElementById('canvas-1');
